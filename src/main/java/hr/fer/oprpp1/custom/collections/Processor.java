4167c2f4e15dac63a66642fb001f5df9e99d7f2a
bp52606 <barbaradirectioner90@gmail.com>
2022-07-17 14:31:25 +0200

    maven project

---
+++ src/main/java/hr/fer/oprpp1/custom/collections/Processor.java
@@ -0,0 +1,18 @@
+package hr.fer.oprpp1.custom.collections;
+/**
+ * Class represents a model of an object capable of performing some operation on the passed object
+ * 
+ * @author 38591
+ *
+ */
+public class Processor {
+	
+	/**
+	 * Processes given value
+	 * 
+	 * @param value Object whose value is being processed
+	 */
+	
+	public void process(Object value) {}
+	
+}
